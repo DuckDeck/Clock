25b0898c60a87c7c6522e86d948090f13b7a71f8
Stan <3421902@qq.com>
2016-03-02 22:25:42 +0800

    upload the project

---
+++ Clock/Clock/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace Clock
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        DispatcherTimer timer = new DispatcherTimer() { Interval = TimeSpan.FromSeconds(1) };
+        DispatcherTimer timerDate = new DispatcherTimer() { Interval = TimeSpan.FromHours(1) };
+        public MainPage()
+        {
+            this.InitializeComponent();
+            timer.Tick += Timer_Tick;
+            timer.Start();
+            timerDate.Tick += TimerDate_Tick;
+            timerDate.Start();
+            tbxDate.Text = DateTime.Now.ToString("yyyy:MM:dd") + " " + DateTime.Now.DayOfWeek.ToString().Substring(0, 3);
+        }
+
+        private void TimerDate_Tick(object sender, object e)
+        {
+            tbxDate.Text = DateTime.Now.ToString("yyyy:MM:dd") +" " + DateTime.Now.DayOfWeek.ToString().Substring(0,3);
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            TimerDisplayer.Time = DateTime.Now;
+        }
+
+        private void MiliSecondHandCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+            MiliSecondHandStoryboard.Begin();
+            MiliSecondHandStoryboard.Seek(DateTime.Now.TimeOfDay);
+        }
+
+        private void TenthsSecondHandCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+            TenthsSecondHandStoryboard.Begin();
+            TenthsSecondHandStoryboard.Seek(DateTime.Now.TimeOfDay);
+        }
+
+     
+
+        private void SecondsHandCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+            SecondsHandStoryboard.Begin();
+            SecondsHandStoryboard.Seek(DateTime.Now.TimeOfDay);
+        }
+        private void MinutesHandCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+            MinutesHandStoryboard.Begin();
+            MinutesHandStoryboard.Seek(DateTime.Now.TimeOfDay);
+        }
+        private void HoursHandCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+            HoursHandStoryboard.Begin();
+            HoursHandStoryboard.Seek(DateTime.Now.TimeOfDay);
+        }
+    }
+}
---
+++ Clock/Clock/TimeDisplay.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Clock
+{
+    public sealed partial class TimeDisplay : UserControl
+    {
+        DateTime? time;
+        DispatcherTimer timer = new DispatcherTimer() { Interval = TimeSpan.FromMilliseconds(500) };
+        public TimeDisplay()
+        {
+            this.InitializeComponent();
+            time = null;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            this.tbxSecondColon.Visibility = this.tbxSecondColon.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+        }
+        public DateTime? Time
+        {
+            get { return this.time; }
+            set
+            {
+                this.time = value;
+                // if the time is null
+                if (this.time == null)
+                {
+                    // Clear everything
+                    this.TimeRun.Text = null;
+                    this.SecondsRun.Text = null;
+                    //this.AMTextBlock.Opacity = .1;
+                    //this.PMTextBlock.Opacity = .1;
+                    return;
+                }
+                string formatString = "HH:mm";
+                // The hour needs a leading space if it ends up being only one digit
+                // when show 24 hours , the hour < 10 need a space or do not show 24 hours ,when time between 12-22 and  0-10  need a space
+                //if ((this.time.Value.Hour < 10) || ((this.time.Value.Hour % 12 < 10 && this.time.Value.Hour % 12 > 0)))
+                //    formatString = " " + formatString;
+                this.TimeRun.Text = this.time.Value.ToString(formatString);
+                 this.SecondsRun.Text = this.time.Value.ToString("ss");
+            }
+
+        }
+    }
+}
